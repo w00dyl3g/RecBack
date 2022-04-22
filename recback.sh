13284c0d3a632078bb51bb2da31734bc7dfa544e
w00dyl3g <giacomosighinolfi@gmail.com>
2022-04-22 14:34:52 +0200

    edit

--- recback.sh
+++ recback.sh
@@ -3,5 +3,13 @@
 banner () {
  cat $(pwd)/banner.txt
 }
+step ()
+{
+  echo "+------------------------------------------+"
+  printf "| %-40s |\n" "`date`"
+  echo "|                                          |"
+  printf "|`tput bold` %-40s `tput sgr0`|\n" "$@"
+  echo "+------------------------------------------+"
+}
 
 banner
