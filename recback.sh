92673ab9a3502d16dbd6db7a2cb91de2641a20e1
w00dyl3g <giacomosighinolfi@gmail.com>
2022-05-03 04:07:24 -0400

    .

--- recback.sh
+++ recback.sh
@@ -1,14 +1,60 @@
 #!/bin/bash
 
+#VARS
+targets=""
 tools=("nmap" "gobuster" "nikto" "nuclei" "sslscan" "whatweb" "dirsearch" "wfuzz" "sqlmap" "nonexist")
+web_tools=("gobuster" "dirsearch" "wfuzz" "ffuf")
+
+
+#COLOR
+R='\033[0;31m'
+G='\033[0;32m'
+O='\033[0;33m'
+NC='\033[0m' # No Color
+
+#FUNCTIONS
 
 banner () {
  cat $(pwd)/banner.txt
 }
 
-for tool in ${tools[@]}; do
-  which $tool
-  if [ "$?" -eq 0 ]; then
-    echo ok
+check_args() {
+ echo -e "$O[!] Parsing command line arguments...$NC"
+ sleep 0.1
+ #arg1 = targets file
+ if [ "$#" -gt 0 ];then
+  echo -e "$G[OK] targets file arg ok!$NC"
+  #check file exists
+  if [ -f "$1" ]; then
+   echo -e "$G[OK] targets file exists!$NC"
+   targets=$1
+   echo $targets
+  else
+   echo -e "$R[NO] targets file not exists, leaving...$NC"
+   return -1
   fi
-done
+ else
+  echo -e "$R[NO] targets file arg missing, leaving...$NC"
+  return -1
+ fi
+}
+
+check_nmap() {
+ echo -e "$O[!] Check if nmap is installed...$NC"
+ which nmap 1>/dev/null
+ sleep 0.1
+ if [ "$?" -eq 0 ]; then
+  echo -e "$G[OK] nmap is installed!$NC"
+ else
+  echo -e "$R[NO] nmap is not installed, leaving...$NC"
+  return -2
+ fi
+}
+
+do_nmap(){
+ sleep 0.1
+}
+
+banner
+check_args $@
+check_nmap
